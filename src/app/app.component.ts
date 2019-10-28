fca9771a5d4eb030839869a04bbe38c137f964c9
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2019-10-27 21:44:13 -0300

    instance ErrorMsgComponent in app.component with ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { ErrorMsgComponent } from './compartilhado/error-msg/error-msg.component';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  titulo = 'CRUD de lembretes com Angular';
+export class AppComponent implements OnInit{
+	titulo = 'CRUD de lembretes com Angular';
+	@ViewChild(ErrorMsgComponent, {static: true}) errorMsgComponent: ErrorMsgComponent;
+
+	ngOnInit(){
+		this.errorMsgComponent.setError('Mensagem de Erro!');
+	}	
 }
+
--- src/app/compartilhado/error-msg/error-msg.component.ts
+++ src/app/compartilhado/error-msg/error-msg.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-error-msg',
   templateUrl: './error-msg.component.html',
   styleUrls: ['./error-msg.component.css']
 })
-export class ErrorMsgComponent implements OnInit {
+export class ErrorMsgComponent {
+	public error: string;
 
-  constructor() { }
+  setError(error: string, tempo: number = 5000) {
+		this.error = error;
+		setTimeout(() => {
+			this.error = null;
+		}, tempo)
+	 }
 
-  ngOnInit() {
-  }
+  
 
 }
